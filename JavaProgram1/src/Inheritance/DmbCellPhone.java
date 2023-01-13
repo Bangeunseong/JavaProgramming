af5442c16fd73110c31537f6404df7e0f89042fa
Bangeunseong <bangeunseong@naver.com>
2023-01-13 15:30:40 +0900

    Commit

---
+++ JavaProgram1/src/Inheritance/DmbCellPhone.java
@@ -0,0 +1,18 @@
+package Inheritance;
+
+public class DmbCellPhone extends CellPhone {
+    int Channel;
+
+    //Constructor
+    DmbCellPhone(String model, String color, int Channel){
+        super(model, color); this.Channel = Channel;
+    }
+
+    //Method
+    void TurnOnDmb(){System.out.println("Channel Number" + Channel +" receive starting");}
+    void ChangeChannelDmb(int Channel){
+        this.Channel = Channel;
+        System.out.println("Changing channel to " + Channel);
+    }
+    void TurnOffDmb(){System.out.println("Stop receiving Dmb Broadcast");}
+}

89b28afac668fe4858a06aa637d88361bf8517ed
Bangeunseong <bangeunseong@naver.com>
2023-01-15 20:01:13 +0900

    Commit

--- JavaProgram1/src/Inheritance/Child.java
+++
@@ -1,7 +0,0 @@
-package Inheritance;
-
-public class Child extends Parent {
-    private String name;
-    public Child(){this("Unknown");}
-    public Child(String name){this.name = name;}
-}
--- JavaProgram1/src/Inheritance/Parent.java
+++
@@ -1,8 +0,0 @@
-package Inheritance;
-
-public class Parent {
-    public String nation;
-
-    public Parent(){this("nation");}
-    public Parent(String nation){this.nation = nation;}
-}

415b6ebb38a52035e191599d7237b0452c48045a
Bangeunseong <bangeunseong@naver.com>
2023-01-15 13:27:50 +0900

    Commit

---
+++ JavaProgram1/src/Inheritance/HankookTire.java
@@ -0,0 +1,24 @@
+package Inheritance;
+
+public class HankookTire extends Tire {
+    private final String name = "HankookTire";
+
+    public HankookTire(String Location, int maxRotation){
+        super(Location, maxRotation);
+    }
+
+    @Override
+    public boolean roll(){
+        if(maxRotation > ++accumulateRotation){
+            System.out.println(location + " " + name + " Life span: " + (maxRotation - accumulateRotation) + "times");
+            return true;
+        }
+        else{
+            System.out.println("*** " + location + " " + name + " punctured ***");
+            return false;
+        }
+    }
+
+    @Override
+    public String getTireName(){return name;}
+}
